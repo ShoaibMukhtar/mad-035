a8ad12a2e01af59e7cbc2b5046bd3da82fb09b1a
ShoaibMukhtar <97965853+ShoaibMukhtar@users.noreply.github.com>
2023-04-02 19:55:29 +0500

    2nd Comit

---
+++ shoaib_xylophone_app/lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Xylophone'),
+        ),
+        body: Xylo(),
+      ),
+    );
+  }
+}
+
+class Xylo extends StatefulWidget {
+  const Xylo({Key? key}) : super(key: key);
+
+  @override
+  State<Xylo> createState() => _XyloState();
+}
+
+class _XyloState extends State<Xylo> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
